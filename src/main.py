06e0a8dbb72a5fb48c7b88fc6c0f494ba94358f2
Landen Doty <landen.doty@outlook.com>
2024-11-03 22:51:41 -0600

    example main and synthesis driver with current impl

--- src/main.py
+++ src/main.py
@@ -4,7 +4,27 @@
 from synthesizer import SynthDriver
 
 if __name__ == "__main__":
-    data = [["1 lb", "23 g", "7 oz"]]
+    #data = [["1 lb", "23 g", "7 oz", "12 kg"]]
+    input_data = [
+        [
+            "Mumbai, India",
+            "Los Angeles, USA",
+            "Newark, USA",
+            "New York, USA",
+            "Wellington, New Zeland"
+        ]
+    ]
+
+    output_data = [
+        "India",
+        "USA",
+        "USA",
+        "USA",
+        "New Zeland"
+    ]
+
     synth = SynthDriver()
-    IDG = synth.GenInpDataGraph(data)
+    IDG = synth.GenInpDataGraph(input_data)
     print(IDG)
+    DAG = synth.GenDag(input_data[0], output_data[0], IDG)
+    print(DAG)
--- src/synthesizer.py
+++ src/synthesizer.py
@@ -50,5 +50,5 @@ class SynthDriver:
     def GenDag(self, inp_data: list, output: str, IDG: 'IDG') -> DAG:
         """ Generate a DAG from a row of input/output example and IDG """
         dag = DAG(len(inp_data))
-        dag.learn(inp_data, output)
+        dag.learn(inp_data, output, IDG)
         return dag
