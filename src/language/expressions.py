44ef37e3264073bcb95e993151cd1ac7d98ff3d1
Landen Doty <landen.doty@outlook.com>
2024-11-18 13:25:27 -0600

    overriding equality and hash methods for expressions
    - for convenience with set operations
    - sets in python are implemented over dictionaries, so these methods
      are required
    - adhering to immutability since expressions are never modified in
      our implementation

--- src/language/expressions.py
+++ src/language/expressions.py
@@ -33,8 +33,17 @@ class ConstStringExpr(Expr):
     ):
         self.const_str = string
 
+    def __key(self):
+        return self.const_str
+
+    def __hash__(self):
+        return hash(self.__key())
+
     def __repr__(self) -> str:
         return f"ConstStr({self.const_str})"
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
 
 class SubStringExpr(Expr):
     """ Substring Expression """
@@ -51,6 +60,31 @@ class SubStringExpr(Expr):
     def __repr__(self) -> str:
         return f"SubStr(\"{self.v}\", {self.pl.__repr__()}, {self.pr.__repr__()}"
 
+    def __hash__(self):
+        return id(self)
+
+    def __eq__(self, other):
+        if isinstance(other, SubStringExpr):
+            return self.pl == other.pl and self.pr == other.pr
+        return False
+
+    @staticmethod
+    def interesct(substr1, substr2) -> 'SubStringExpr':
+        """ intersect two substring expressions """
+        left = substr1.pl & substr2.pl
+        if len(left) == 0:
+            return None
+
+        right = substr1.pr & substr2.pr
+        if len(right) == 0:
+            return None
+
+        return SubStringExpr(
+                    substr = f"{substr1.v} + {substr2.v}",
+                    left=left,
+                    right=right
+                )
+
 class PosExpr(Expr):
     """ Position Expression w/ Token """
     def __init__(self,
@@ -65,6 +99,19 @@ class PosExpr(Expr):
     def __repr__(self) -> str:
         return f"PosExpr(\"{self.tok}\",{self.idx},{self.direction.value})"
 
+    def __key(self):
+        return (self.tok,self.idx,self.direction.value)
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __eq__(self, other):
+        if isinstance(other, PosExpr):
+            return self.tok == other.tok  and \
+                    self.idx == other.idx and \
+                    self.direction == other.direction
+        return False
+
 class ConstPosExpr(Expr):
     """ Constant Position Expression """
     def __init__(self,
@@ -74,6 +121,17 @@ class ConstPosExpr(Expr):
 
     def __repr__(self) -> str:
         return f"ConstPos({self.idx})"
+
+    def __key(self) -> int:
+        return self.idx
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __eq__(self, other):
+        if isinstance(other, ConstPosExpr):
+            return self.idx == other.idx
+        return False
 
 class Direction(Enum):
     Start = "Start"
