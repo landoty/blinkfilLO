3f5c9152cc68c54617fba3f7403f93b6987e2c38
Landen Doty <landen.doty@outlook.com>
2024-11-18 20:41:09 -0600

    adding benchmarks

---
+++ eval/README.md
@@ -0,0 +1,3 @@
+# Evaluation Benchmarks
+
+We evaluate BlinkFilLO using a subset of the `Transformation` set from the [Prose Benchmarks](git@github.com:microsoft/prose-benchmarks.git). Specifically, we choose benchmarks from this set which include *only* the "concatenation", "multicol", and "substring" features. This subset is representative of all the program types that the original BlinkFilLO supports. 
---
+++ eval/filter.py
@@ -0,0 +1,24 @@
+""" Quick script to filter the correct benchmarks """
+# eval/filter.py
+
+import os
+import shutil
+import json
+
+for benchmark in os.listdir("."):
+    if not os.path.isdir(benchmark):
+        continue
+
+    meta = os.path.join(os.path.abspath(benchmark), "meta.json")
+    remove = False
+    with open(meta) as f:
+        config = json.load(f)
+        for ft in config["Features"]:
+            if ft.lower() != "concatenation" and \
+                ft.lower() != "multicol" and \
+                ft.lower() != "substring":
+                remove = True
+                break
+
+    if remove:
+        shutil.rmtree(os.path.abspath(benchmark))
